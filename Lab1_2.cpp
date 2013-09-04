ef59b7618cdb7de6e0740caf913d607b2145cf6f
unknown <dmitriyn@ws-chl104b.chelyabinsk.fuse8.local>
2013-09-04 10:39:08 +0600

    Добавлены cpp файлы с шаблонами

---
+++ Lab1_2.cpp
@@ -0,0 +1,15 @@
+#include "StdAfx.h"
+#include "Lab1_2.h"
+
+Lab1_2::Lab1_2(void)
+{
+}
+
+Lab1_2::~Lab1_2(void)
+{
+}
+
+SomeMethod()
+{
+	SomeField = 0;
+}
---
+++ Lab1_2.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Lab1_2
+{
+public:
+	Lab1_2(void);
+	~Lab1_2(void);
+	SomeMethod();
+	int SomeField;
+};
