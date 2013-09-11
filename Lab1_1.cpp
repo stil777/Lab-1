a9f8de0d3b74f0324e8fce42dd81062d25eab997
ILYA Starodubov <stil3113@mail.ru>
2013-09-11 23:10:27 +0600

    Update Lab1_1.cpp

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -1,33 +1,26 @@
-��// Test.cpp : Defines the entry point for the console application.
-
+// Lab1.cpp: определяет точку входа для консольного приложения.
 //
 
-
-
 #include "stdafx.h"
-
-
-
+#include <conio.h>
+#include <locale.h>
 
 
 int _tmain(int argc, _TCHAR* argv[])
-
 {
+	setlocale(LC_ALL,".1251");
+	int counter, denominator;
 
-int counter, denominator;
+	counter = 0; //Устанавливаем начальные значение
+	denominator = 1;
+	printf("Увеличиваем счетчик и отображаем на экране\n");
+	printf("%d\n",++counter);
+	printf("Вычисляем значение Counter/Denominator = ");
+	printf("%d\n",counter/denominator);
+	printf("%d",counter);
 
-
-
-counter = 0; //#AB0=02;8205< =0G0;L=K5 7=0G5=85
-denominator = 1;
-
-
-printf("Увеличиваем счетчик и отображаем на экране\n");
-printf("%d\n",++counter);
-printf("Вычисляем значение Counter/Denominator = ");
-printf("%d\n",counter/denominator);
-printf("%d",counter);
-
+	getch();
+	return 0;
 }
 
 
