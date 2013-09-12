fc2b473f9b9e661ab3cad0c8627dfd0a9a029d4b
ILYA Starodubov <stil3113@mail.ru>
2013-09-12 09:23:36 +0600

    Update Lab1_2.cpp

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -1,40 +1,50 @@
 #include "StdAfx.h"
 #include "Lab1_2.h"
 #include <string>
+#include <conio.h>
 
-void Students::put_name(std::string student_name)
+void Student::put_name(std::string student_name)
 {
-    Students::name = student_name;
+    Student::name = student_name;
 }
 
-std::string Students::get_name()
+std::string Student::get_name()
 {
-    return Students::name;
+    return Student::name;
 }
 
-void Students::put_last_name(std::string student_last_name)
+void Student::put_last_name(std::string student_last_name)
 {
-    Students::last_name = student_last_name;
+    Student::last_name = student_last_name;
 }
 
-std::string Students::get_last_name()
+std::string Student::get_last_name()
 {
-    return Students::last_name;
+    return Student::last_name;
 }
 
-void Students::set_scores(int scores[])
+void Student::put_scores(int scores[])
 {
     for (int i = 0; i < 5; ++i) {
-        Students::scores[i] = scores[i];
+        Student::scores[i] = scores[i];
     }
 }
 
-void Students::set_average_ball(float ball)
+void Student::put_average_ball(float ball)
 {
-    Students::average_ball = ball;
+    Student::average_ball = ball;
 }
 
-float Students::get_average_ball()
+float Student::get_average_ball()
 {
-    return Students::average_ball;
+    return Student::average_ball;
+}
+
+int main()
+{
+    Student *student1 = new Student(0);
+    delete student1;
+
+	getch();
+    return 0;
 }
