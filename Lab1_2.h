cafc8df76d37c46d89151e281b30c9e0f303aa58
ILYA Starodubov <stil3113@mail.ru>
2013-09-12 08:53:10 +0600

    Update Lab1_2.h

--- Lab1_2.h
+++ Lab1_2.h
@@ -1,10 +1,15 @@
 #pragma once
 
-class Lab1_2
+class Student
 {
 public:
-	Lab1_2(void);
-	~Lab1_2(void);
-	SomeMethod();
-	int SomeField;
+	Student(void);
+	~Student(void);
+	void put_name(std::string); //Установка имени студента
+        std::string get_name(); //Получение имени студента
+        void put_last_name(std::string); //Установка фамилии студента
+        std::string get_last_name(); //Получение фамилии студента
+        void put_scores(int []); //Установка промежуточных оценок
+        void set_average_ball(float); //Установка среднего балла
+        float get_average_ball(); //Получение среднего балла
 };
