f0ddea356859752421ffec6e4e8d5c98d4d8814b
ILYA Starodubov <stil3113@mail.ru>
2013-09-12 09:07:52 +0600

    Update Lab1_2.cpp

--- Lab1_2.cpp
+++ Lab1_2.cpp
@@ -2,7 +2,7 @@
 #include "Lab1_2.h"
 #include <string>
 
-void Students::set_name(std::string student_name)
+void Students::put_name(std::string student_name)
 {
     Students::name = student_name;
 }
@@ -10,4 +10,31 @@ void Students::set_name(std::string student_name)
 std::string Students::get_name()
 {
     return Students::name;
+}
+
+void Students::put_last_name(std::string student_last_name)
+{
+    Students::last_name = student_last_name;
+}
+
+std::string Students::get_last_name()
+{
+    return Students::last_name;
+}
+
+void Students::set_scores(int scores[])
+{
+    for (int i = 0; i < 5; ++i) {
+        Students::scores[i] = scores[i];
+    }
+}
+
+void Students::set_average_ball(float ball)
+{
+    Students::average_ball = ball;
+}
+
+float Students::get_average_ball()
+{
+    return Students::average_ball;
 }
