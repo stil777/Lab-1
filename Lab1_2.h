cbbe60521b9fe3755b5a8862706af1267332b96e
ILYA Starodubov <stil3113@mail.ru>
2013-09-12 09:24:31 +0600

    Update Lab1_2.h

--- Lab1_2.h
+++ Lab1_2.h
@@ -12,15 +12,15 @@ public:
             }
         }
 	~Student(void)
-	{
-		std::cout << "The work is over, memory cleaned succesfuly." << std::endl;
+	{ 
+		printf("The work is over, memory cleaned succesfuly.");
 	}
 	void put_name(std::string); //Установка имени студента
         std::string get_name(); //Получение имени студента
         void put_last_name(std::string); //Установка фамилии студента
         std::string get_last_name(); //Получение фамилии студента
         void put_scores(int []); //Установка промежуточных оценок
-        void set_average_ball(float); //Установка среднего балла
+        void put_average_ball(float); //Установка среднего балла
         float get_average_ball(); //Получение среднего балла
 private:
         int scores[5]; // Промежуточные оценки
